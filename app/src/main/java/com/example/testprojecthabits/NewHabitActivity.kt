3e204df4778c0cfb4ae6ced973ba7a9b12977ea3
danilov032 <danilov032@mail.ru>
2021-09-22 21:16:04 +0300

    Накидал дизайн

---
+++ app/src/main/java/com/example/testprojecthabits/NewHabitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testprojecthabits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewHabitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_habit)
+    }
+}
